a5b864215f440a0e660451154a3e6f77df0dde97
Ailisun <13prichin@gmail.com>
2022-04-15 00:30:00 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import './App.css'
-import Actor from "./components/Actor";
-import Users from "./components/Users";
+const App = () => {
+    return (
+        <div>
+          
+        </div>
+    );
+};
 
-
-const actors = [
-  {id:1, name:'Tom', img:'https://shutnikov.club/wp-content/uploads/2020/01/tom-cat-my-talking-tom-jerry-mouse-tom-and-jerry-tom-and-jerry-png.jpg'},
-  {id:2, name:'Jerry', img:'https://pngimg.com/uploads/tom_and_jerry/tom_and_jerry_PNG56.png'}
-]
-function App(){
-  return (<div>
-    {/*{actors.map(actor=><Actor key={actor.id} name = {actor.name} img = {actor.img}/>)}*/}
-       <Users/>
-  </div>
-  )
-}
 export default App;
