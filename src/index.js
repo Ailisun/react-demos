8442cb1270747ec3c6f8a0dd055617c8afa8fd6d
Ailisun <13prichin@gmail.com>
2022-05-03 00:06:26 +0300

    base part 1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 
+function reducer (state = 0, action) {
+    switch (action.type){
+        case 'INC':
+            return state + action.payload
+        case 'DEC':
+            return state - action.payload
+        case 'reset':
+            return state = action.payload
+        default:
+            return state
+    }
+
+}
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
+let store = createStore(reducer)
+root.render(<Provider store={store}><App/></Provider>)
