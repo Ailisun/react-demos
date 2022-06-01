268e87f1e5a220d7bdf6ed219d464a04f3cc727f
Ailisun <13prichin@gmail.com>
2022-06-01 18:01:32 +0300

    improve

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import Navigation from './components/Navigation'
 const App = () => {
     return (
         <div>
-          <h1>MovieFinder</h1>
-            <Navigation> </Navigation>
+
         </div>
     );
 };
